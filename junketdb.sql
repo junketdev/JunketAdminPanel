5ce809b14bfc1acabbf2fa213dac7625dd0c6d6c
junketdev <junketdev@users.noreply.github.com>
2020-01-23 11:58:48 +0530

    Update junketdb

--- junketdb.sql
+++ junketdb.sql
@@ -1,26 +1,26 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Aug 10, 2019 at 06:54 AM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.16
+-- Host: 127.0.0.1
+-- Generation Time: Jan 23, 2020 at 05:42 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `junketdb`
 --
-CREATE DATABASE IF NOT EXISTS `junketdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `junketdb`;
 
 -- --------------------------------------------------------
 
@@ -28,12 +28,23 @@ USE `junketdb`;
 -- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `admin_id` int(12) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `admin_id` int(12) NOT NULL,
   `admin_name` varchar(50) NOT NULL,
-  `admin_password` varchar(20) NOT NULL,
-  PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `admin_password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attraction`
+--
+
+CREATE TABLE `attraction` (
+  `name` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `images` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -41,7 +52,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
 -- Table structure for table `booking`
 --
 
-CREATE TABLE IF NOT EXISTS `booking` (
+CREATE TABLE `booking` (
   `user_id` int(12) NOT NULL,
   `booking_id` varchar(12) NOT NULL,
   `destination` varchar(20) NOT NULL,
@@ -56,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `booking` (
 -- Table structure for table `complain`
 --
 
-CREATE TABLE IF NOT EXISTS `complain` (
+CREATE TABLE `complain` (
   `comp_id` int(12) NOT NULL,
   `comp_date` date NOT NULL,
   `user_id` int(12) NOT NULL,
@@ -64,28 +75,79 @@ CREATE TABLE IF NOT EXISTS `complain` (
   `comp_status` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `complain`
+--
+
+INSERT INTO `complain` (`comp_id`, `comp_date`, `user_id`, `comp_detail`, `comp_status`) VALUES
+(25, '0000-00-00', 45, 'uhdiooidofjsdofofjdsjf', ''),
+(45, '2020-01-10', 56, 'ytddfghjkkjhgfdfghj', ''),
+(0, '2020-01-12', 45, 'not good', ''),
+(0, '2020-01-12', 45, 'not good', 'regis');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `properties`
+-- Table structure for table `destination`
 --
 
-CREATE TABLE IF NOT EXISTS `properties` (
-  `property_id` int(12) NOT NULL AUTO_INCREMENT,
-  `property_name` varchar(50) NOT NULL,
-  `property_type` varchar(25) NOT NULL,
+CREATE TABLE `destination` (
+  `description` varchar(255) NOT NULL,
+  `latitude` varchar(25) NOT NULL,
+  `longitude` varchar(25) NOT NULL,
+  `images` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `name` varchar(250) NOT NULL,
+  `type` varchar(250) NOT NULL,
+  `price` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `images` varchar(250) NOT NULL,
+  `destination` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotelier`
+--
+
+CREATE TABLE `hotelier` (
+  `hotelier_id` int(12) NOT NULL,
+  `hotelier_name` varchar(50) NOT NULL,
+  `hotelier_password` varchar(255) NOT NULL,
+  `hotelier_type` varchar(25) NOT NULL,
   `city` varchar(25) NOT NULL,
+  `hotelier_address` varchar(255) NOT NULL,
   `longitude` varchar(25) NOT NULL,
   `latitude` varchar(25) NOT NULL,
   `description` varchar(250) DEFAULT NULL,
-  `contact` int(12) NOT NULL,
-  `email` varchar(25) NOT NULL,
+  `contact` varchar(50) NOT NULL,
+  `hotelier_email` varchar(255) NOT NULL,
   `rating` varchar(5) DEFAULT NULL,
   `amenities` varchar(250) NOT NULL,
   `offers` varchar(250) DEFAULT NULL,
-  `image` varchar(250) NOT NULL,
-  PRIMARY KEY (`property_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `image` varchar(255) NOT NULL,
+  `website` varchar(255) NOT NULL,
+  `min_price` varchar(255) NOT NULL,
+  `max_price` varchar(255) NOT NULL,
+  `hotelier_tags` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hotelier`
+--
+
+INSERT INTO `hotelier` (`hotelier_id`, `hotelier_name`, `hotelier_password`, `hotelier_type`, `city`, `hotelier_address`, `longitude`, `latitude`, `description`, `contact`, `hotelier_email`, `rating`, `amenities`, `offers`, `image`, `website`, `min_price`, `max_price`, `hotelier_tags`) VALUES
+(1, 'Royal Orchid', 'royal_orchid', '4', 'Vadodara', 'Mujmahuda Rd, Pratham Avenue, Akota, Vadodara', '22.286892', '73.168939', 'Not just for archaeological fans, Vadodara is a city of interest for many travelers. Known for its rich culture, glorious past and royal traditions, this city is a perfect location for a quick escapade. Royal Orchid Central, Vadodara is located near ', '0265-2358679', 'help_regenta@gmail.com', '4.2', 'Swimming pool, Gym , Lavish rooms ', NULL, '', 'https://www.royalorchidhotels.com/', '3326', '5600', 'lavish\r\nIn city\r\nMeeting Friendly\r\nConference Room\r\n'),
+(10245, 'roay orchid', 'royalorchid', '4', 'Vadodara', 'Mujhmuda road', '22.286892', '72.56698', 'Lavish in-city hotel with ample of comfort', '0265-235871', 'royalorchidhotels@gmail.com', '5', 'Pool\r\nConference Room\r\nBanquote Hall (500 pax)\r\nPet Friendly', '10% discount for newly weds', '', 'regentahotels.com', '3200', '5799', 'In-City\r\nHoneymoon\r\nAmbience\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n');
 
 -- --------------------------------------------------------
 
@@ -93,15 +155,22 @@ CREATE TABLE IF NOT EXISTS `properties` (
 -- Table structure for table `questionnaire`
 --
 
-CREATE TABLE IF NOT EXISTS `questionnaire` (
-  `qid` int(12) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `questionnaire` (
+  `qid` int(12) NOT NULL,
   `question` varchar(100) NOT NULL,
   `option1` varchar(50) NOT NULL,
   `option2` varchar(50) NOT NULL,
   `option3` varchar(50) NOT NULL,
-  `option4` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`qid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `option4` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questionnaire`
+--
+
+INSERT INTO `questionnaire` (`qid`, `question`, `option1`, `option2`, `option3`, `option4`) VALUES
+(45, 'bknkjbjv', 'jbbk', 'bb', 'kbb', 'jbjb'),
+(78, 'jhvhb', 'bjhvh', 'jhvjv', 'hjh', 'jhjh');
 
 -- --------------------------------------------------------
 
@@ -109,13 +178,12 @@ CREATE TABLE IF NOT EXISTS `questionnaire` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(12) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `user_id` int(12) NOT NULL,
   `user_password` varchar(20) NOT NULL,
-  `contact_no` int(12) NOT NULL,
-  `email` varchar(25) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `contact_no` int(20) NOT NULL,
+  `email` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -123,14 +191,119 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Table structure for table `user_details`
 --
 
-CREATE TABLE IF NOT EXISTS `user_details` (
-  `user_id` int(12) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user_details` (
+  `user_id` int(12) NOT NULL,
   `user_name` varchar(50) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `dob` date NOT NULL,
-  `profile_pic` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `profile_pic` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `user_id` int(12) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contact_no` varchar(255) NOT NULL,
+  `dob` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `profile_pic` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`user_id`, `user_name`, `email`, `contact_no`, `dob`, `gender`, `user_password`, `profile_pic`) VALUES
+(1, 'Test', 'pat@213', '694585', '09/01/1998', 'Male', 'xys', 'boiklk'),
+(2, '', '', '', '', '', '', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `hotelier`
+--
+ALTER TABLE `hotelier`
+  ADD PRIMARY KEY (`hotelier_id`);
+
+--
+-- Indexes for table `questionnaire`
+--
+ALTER TABLE `questionnaire`
+  ADD PRIMARY KEY (`qid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_details`
+--
+ALTER TABLE `user_details`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hotelier`
+--
+ALTER TABLE `hotelier`
+  MODIFY `hotelier_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10246;
+
+--
+-- AUTO_INCREMENT for table `questionnaire`
+--
+ALTER TABLE `questionnaire`
+  MODIFY `qid` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_details`
+--
+ALTER TABLE `user_details`
+  MODIFY `user_id` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `user_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
