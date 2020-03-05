bca40896f492d19f113d40464a71a71da261442e
junketdev <junketdev@users.noreply.github.com>
2020-03-05 17:25:58 +0530

    Doc updates with logo

--- get_booking.php
+++ get_booking.php
@@ -3,7 +3,7 @@
 require "connect.php";
 $data=json_decode(file_get_contents('PHP://input'),true);
 $user_id=$data['user_id'];
-$result="SELECT * FROM booking where user_id='" . $user_id ."'";
+$result="SELECT * FROM booking as b INNER JOIN hotelier as h ON b.hotel_id = h.hotel_id INNER JOIN roomlisting as r ON b.room_id = r.room_id where b.user_id='" . $user_id ."'";
 $res1=mysqli_query($conn,$result);
 $value=array();
 
